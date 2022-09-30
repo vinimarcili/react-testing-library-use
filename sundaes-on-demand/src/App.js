85b31516bce357b099fd6ec2d4584a68231efefa
Viní Marcili <vinipetter@gmail.com>
2022-09-30 08:57:34 -0300

    Add final tests

--- sundaes-on-demand/src/App.js
+++ sundaes-on-demand/src/App.js
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 
-// import OrderConfirmation from './pages/confirmation/OrderConfirmation'
+import OrderConfirmation from './pages/confirmation/OrderConfirmation'
 import OrderEntry from './pages/entry/OrderEntry'
-// import OrderSummary from './pages/summary/OrderSummary'
+import OrderSummary from './pages/summary/OrderSummary'
 
 import { OrderDetailsProvider } from './contexts/OrderDetails'
 
@@ -16,12 +16,12 @@ export default function App() {
     case 'inProgress':
       Component = OrderEntry
       break
-    // case 'review':
-    //   Component = OrderSummary
-    //   break
-    // case 'completed':
-    //   Component = OrderConfirmation
-    // break
+    case 'review':
+      Component = OrderSummary
+      break
+    case 'completed':
+      Component = OrderConfirmation
+      break
     default:
   }
 
